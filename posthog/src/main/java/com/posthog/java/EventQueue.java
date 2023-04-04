5ef1eec9e9cbd36fa5de48803923a83aef530fb2
dsinghvi <deep@usebirch.com>
2023-04-04 15:41:16 -0700

    save progress

---
+++ posthog/src/main/java/com/posthog/java/EventQueue.java
@@ -0,0 +1,127 @@
+package com.posthog.java;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.posthog.api.PosthogApiClient;
+import com.posthog.api.core.ObjectMappers;
+import com.posthog.api.resources.requests.BatchEventRequest;
+import com.posthog.api.resources.types.Event;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public final class EventQueue {
+    private static final int MAX_QUEUE_SIZE = 10000;
+
+    private final String apiKey;
+    private final BlockingQueue<Event> queue;
+    private final PosthogApiClient posthogApiClient;
+    private final ExecutorService flusherExecutor;
+    private final long flushIntervalInMillis;
+    private final int flushAt;
+    private final AtomicBoolean isShutDown;
+
+    public EventQueue(
+            String apiKey,
+            PosthogApiClient posthogApiClient,
+            ThreadFactory threadFactory,
+            int flushIntervalInMillis,
+            int flushAt) {
+        this.apiKey = apiKey;
+        this.queue = new LinkedBlockingQueue<>(MAX_QUEUE_SIZE);
+        this.flushAt = flushAt;
+        this.flushIntervalInMillis = flushIntervalInMillis;
+        this.isShutDown = new AtomicBoolean(false);
+        this.posthogApiClient = posthogApiClient;
+        this.flusherExecutor = Executors.newSingleThreadExecutor(threadFactory);
+        this.flusherExecutor.submit(new Flusher());
+    }
+
+    public void enqueue(Event event) {
+        this.queue.add(event);
+    }
+
+    public void shutdown() {
+        if (isShutDown.compareAndSet(false, true)) {
+            try {
+                flusherExecutor.shutdown();
+                flusherExecutor.awaitTermination(1, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    /**
+     * Flusher runs on a background thread and takes messages from the queue. Once it collects enough
+     * messages or enough time has elapsed, it triggers a flush.
+     */
+    class Flusher implements Runnable {
+
+        private static final int MAX_MSG_SIZE = 32 << 10;
+
+        /**
+         * Our servers only accept batches less than 500KB. Here limit is set slightly
+         * # lower to leave space for extra data that will be added later, eg. "sentAt".
+         */
+        private static final int BATCH_SIZE_LIMIT_IN_BYTES = 475000;
+
+        @Override
+        public void run() {
+            while (true) {
+                upload();
+            }
+        }
+
+        private void upload() {
+            List<Event> batch = getBatch();
+            try {
+                posthogApiClient.batchEvents(BatchEventRequest.builder()
+                        .apiKey(apiKey)
+                        .batch(batch)
+                        .build());
+            } catch (Exception e) {
+                // TODO: Log failure
+            }
+        }
+
+        private List<Event> getBatch() {
+            List<Event> events = new ArrayList<>();
+            long start = System.currentTimeMillis();
+
+            while (events.size() < flushAt) {
+                long elapsed = System.currentTimeMillis() - start;
+                if (elapsed > flushIntervalInMillis) {
+                    break;
+                }
+                try {
+                    Event event = queue.poll(flushIntervalInMillis - elapsed, TimeUnit.MILLISECONDS);
+                    int sizeInBytes = getMessageSizeInBytes(event);
+                    if (sizeInBytes > MAX_MSG_SIZE) {
+                        continue;
+                    }
+                    events.add(event);
+                    if (events.size() > BATCH_SIZE_LIMIT_IN_BYTES) {
+                        break;
+                    }
+                } catch (InterruptedException | JsonProcessingException e) {
+                    // TODO: Log failure
+                    break;
+                }
+            }
+            return events;
+        }
+    }
+
+    private static int getMessageSizeInBytes(Event event) throws JsonProcessingException {
+        String stringifiedMessage = ObjectMappers.JSON_MAPPER.writeValueAsString(event);
+        return stringifiedMessage.getBytes(StandardCharsets.UTF_8).length;
+    }
+
+}
--- posthog/src/main/java/com/posthog/java/PostHog.java
+++ posthog/src/main/java/com/posthog/java/PostHog.java
@@ -1,134 +1,133 @@
 package com.posthog.java;
 
+import com.posthog.api.PosthogApiClient;
+import com.posthog.api.core.Environment;
+import com.posthog.api.resources.feature.flags.requests.DecideFeatureFlagsRequest;
+import com.posthog.api.resources.feature.flags.types.DecideFeatureFlagsResponse;
+import com.posthog.api.resources.types.AliasEvent;
+import com.posthog.api.resources.types.AliasEventProperties;
+import com.posthog.api.resources.types.CaptureEvent;
+import com.posthog.api.resources.types.Event;
+import com.posthog.api.resources.types.GroupIdentifyEvent;
+import com.posthog.api.resources.types.GroupIdentifyEventProperties;
+import com.posthog.api.resources.types.IdentifyEvent;
+import com.posthog.api.resources.types.PageViewEvent;
+import com.posthog.api.resources.types.SetEvent;
+import com.posthog.api.resources.types.SetOnceEvent;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
+import java.util.concurrent.Executors;
+import jdk.javadoc.internal.doclint.Env;
+import jdk.tools.jlink.internal.Platform;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class PostHog {
-    private QueueManager queueManager;
-    private Thread queueManagerThread;
 
-    private static abstract class BuilderBase {
-        protected QueueManager queueManager;
-    }
+    private PosthogApiClient posthogApiClient;
+    private EventQueue queue;
 
-    public static class Builder extends BuilderBase {
+    public static class Builder {
         // required
         private final String apiKey;
+        private int flushAt = 100;
+        private int flushIntervalMillis = 500;
 
         // optional
-        private String host = "https://app.posthog.com";
+        private Environment environment = Environment.CLOUD;
 
         public Builder(String apiKey) {
             this.apiKey = apiKey;
         }
 
         public Builder host(String host) {
-            this.host = host;
+            this.environment = Environment.custom(host);
+            return this;
+        }
+
+        public Builder flushAt(int flushAt) {
+            this.flushAt = flushAt;
+            return this;
+        }
+
+        public Builder flushIntervalMillis(int flushIntervalMillis) {
+            this.flushIntervalMillis = flushIntervalMillis;
             return this;
         }
 
         public PostHog build() {
-            Sender sender = new HttpSender.Builder(apiKey).host(host).build();
-            this.queueManager = new QueueManager.Builder(sender).build();
             return new PostHog(this);
         }
     }
 
-    public static class BuilderWithCustomQueueManager extends BuilderBase {
-
-        public BuilderWithCustomQueueManager(QueueManager queueManager) {
-            this.queueManager = queueManager;
-        }
-
-        public PostHog build() {
-            return new PostHog(this);
-        }
-    }
-
-    private PostHog(BuilderBase builder) {
-        this.queueManager = builder.queueManager;
-        startQueueManager();
+    private PostHog(Builder builder) {
+        this.posthogApiClient = PosthogApiClient.builder()
+                .environment(builder.environment)
+                .build();
+        this.queue = new EventQueue(
+                builder.apiKey,
+                posthogApiClient,
+                Executors.defaultThreadFactory(),
+                builder.flushIntervalMillis,
+                builder.flushAt);
     }
 
     public void shutdown() {
-        queueManager.stop();
-        try {
-            queueManagerThread.join(); // wait for the current items in queue to be sent
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
-    private void startQueueManager() {
-        queueManagerThread = new Thread(queueManager, "PostHog QueueManager thread");
-        queueManagerThread.start();
-        // TODO handle interrupts? (via addShutdownHook)
-    }
-
-    private void enqueue(String distinctId, String event, Map<String, Object> properties) {
-        JSONObject eventJson = getEventJson(event, distinctId, properties);
-        queueManager.add(eventJson);
+        queue.shutdown();
     }
 
     /**
-     * 
+     *
      * @param distinctId which uniquely identifies your user in your database. Must
      *                   not be null or empty.
      * @param event      name of the event. Must not be null or empty.
      * @param properties an array with any event properties you'd like to set.
      */
     public void capture(String distinctId, String event, Map<String, Object> properties) {
-        enqueue(distinctId, event, properties);
+        CaptureEvent._FinalStage captureEventBuilder = CaptureEvent.builder()
+                .event(event)
+                .distinctId(distinctId)
+                .uuid(UUID.randomUUID())
+                .timestamp(Instant.now().toString());
+        if (properties != null) {
+            captureEventBuilder.properties(properties);
+        }
+        enqueue(Event.of(captureEventBuilder.build()));
     }
 
     /**
-     * 
+     *
      * @param distinctId which uniquely identifies your user in your database. Must
      *                   not be null or empty.
      * @param event      name of the event. Must not be null or empty.
      */
     public void capture(String distinctId, String event) {
-        enqueue(distinctId, event, null);
+        capture(distinctId, event, null);
     }
 
     /**
-     * 
+     *
      * @param distinctId        which uniquely identifies your user in your
      *                          database. Must not be null or empty.
      * @param properties        an array with any person properties you'd like to
-     *                          set.
-     * @param propertiesSetOnce an array with any person properties you'd like to
-     *                          set without overwriting previous values.
-     */
-    public void identify(String distinctId, Map<String, Object> properties, Map<String, Object> propertiesSetOnce) {
-        Map<String, Object> props = new HashMap<String, Object>();
-        if (properties != null) {
-            props.put("$set", properties);
-        }
-        if (propertiesSetOnce != null) {
-            props.put("$set_once", propertiesSetOnce);
-        }
-        enqueue(distinctId, "$identify", props);
-    }
-
-    /**
-     * 
-     * @param distinctId which uniquely identifies your user in your database. Must
-     *                   not be null or empty.
-     * @param properties an array with any person properties you'd like to set.
      */
     public void identify(String distinctId, Map<String, Object> properties) {
-        identify(distinctId, properties, null);
+        IdentifyEvent._FinalStage identifyEventBuilder = IdentifyEvent.builder()
+                .event("$identify")
+                .distinctId(distinctId)
+                .timestamp(Instant.now().toString());;
+        if (properties != null) {
+            identifyEventBuilder.properties(properties);
+        }
+        enqueue(Event.of(identifyEventBuilder.build()));
     }
 
     /**
-     * 
+     *
      * @param distinctId distinct ID to merge. Must not be null or empty. Note: If
      *                   there is a conflict, the properties of this person will
      *                   take precedence.
@@ -137,61 +136,127 @@ public class PostHog {
      *                   overriden.
      */
     public void alias(String distinctId, String alias) {
-        Map<String, Object> props = new HashMap<String, Object>() {
-            {
-                put("distinct_id", distinctId);
-                put("alias", alias);
-            }
-        };
-        enqueue(distinctId, "$create_alias", props);
+        AliasEvent aliasEvent = AliasEvent.builder()
+                .event("$create_alias")
+                .distinctId(distinctId)
+                .properties(AliasEventProperties.builder()
+                        .distinctId(distinctId)
+                        .alias(alias)
+                        .build())
+                .timestamp(Instant.now().toString())
+                .build();
+        enqueue(Event.of(aliasEvent));
     }
 
     /**
-     * 
+     *
      * @param distinctId which uniquely identifies your user in your database. Must
      *                   not be null or empty.
-     * @param properties an array with any person properties you'd like to set.
+     * @param properties a map with any person properties you'd like to set.
      */
     public void set(String distinctId, Map<String, Object> properties) {
-        Map<String, Object> props = new HashMap<String, Object>() {
-            {
-                put("$set", properties);
-            }
-        };
-        enqueue(distinctId, "$set", props);
+        SetEvent._FinalStage setEventBuilder = SetEvent.builder()
+                .event("$set")
+                .distinctId(distinctId)
+                .uuid(UUID.randomUUID())
+                .timestamp(Instant.now().toString());
+        if (properties != null) {
+            setEventBuilder.properties(properties);
+        }
+        enqueue(Event.of(setEventBuilder.build()));
     }
 
     /**
-     * 
+     *
      * @param distinctId which uniquely identifies your user in your database. Must
      *                   not be null or empty.
-     * @param properties an array with any person properties you'd like to set.
+     * @param properties a map with any person properties you'd like to set.
      *                   Previous values will not be overwritten.
      */
     public void setOnce(String distinctId, Map<String, Object> properties) {
-        Map<String, Object> props = new HashMap<String, Object>() {
-            {
-                put("$set_once", properties);
-            }
-        };
-        enqueue(distinctId, "$set_once", props);
+        SetOnceEvent._FinalStage setOnceBuilder = SetOnceEvent.builder()
+                .event("$set_once")
+                .distinctId(distinctId)
+                .uuid(UUID.randomUUID())
+                .timestamp(Instant.now().toString());
+        if (properties != null) {
+            setOnceBuilder.properties(properties);
+        }
+        enqueue(Event.of(setOnceBuilder.build()));
     }
 
-    private JSONObject getEventJson(String event, String distinctId, Map<String, Object> properties) {
-        JSONObject eventJson = new JSONObject();
-        try {
-            // Ensure that we generate an identifier for this event such that we can e.g.
-            // deduplicate server-side any duplicates we may receive.
-            eventJson.put("uuid", UUID.randomUUID().toString());
-            eventJson.put("timestamp", Instant.now().toString());
-            eventJson.put("distinct_id", distinctId);
-            eventJson.put("event", event);
-            if (properties != null) {
-                eventJson.put("properties", properties);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
+    /**
+     *
+     * @param groupType
+     * @param groupKey
+     * @param properties
+     */
+    public void groupIdentify(
+            String groupType,
+            String groupKey,
+            Map<String, Object> properties) {
+        GroupIdentifyEventProperties._FinalStage groupProperties = GroupIdentifyEventProperties.builder()
+                .groupType(groupType)
+                .groupKey(groupKey);
+        if (properties != null) {
+            groupProperties.properties(properties);
         }
-        return eventJson;
+        GroupIdentifyEvent groupIdentifyEvent = GroupIdentifyEvent.builder()
+                .event("$groupidentify")
+                .distinctId(String.format("%s_%s", groupType, groupKey))
+                .properties(groupProperties.build())
+                .uuid(UUID.randomUUID())
+                .timestamp(Instant.now().toString())
+                .build();
+        enqueue(Event.of(groupIdentifyEvent));
+    }
+
+    /**
+     *
+     * @param distinctId which uniquely identifies your user in your database. Must
+     *                   not be null or empty.
+     * @param url
+     * @param properties
+     */
+    public void page(
+            String distinctId,
+            String url,
+            Map<String, Object> properties) {
+        Map<String, Object> propertyCopy = new HashMap<>();
+        if (properties != null) {
+            propertyCopy.putAll(properties);
+        }
+        propertyCopy.put("$current_url", url);
+        PageViewEvent pageViewEvent = PageViewEvent.builder()
+                .event("$pageview")
+                .distinctId(distinctId)
+                .uuid(UUID.randomUUID())
+                .timestamp(Instant.now().toString())
+                .properties(propertyCopy)
+                .build();
+        enqueue(Event.of(pageViewEvent));
+    }
+
+    private void enqueue(Event event) {
+        queue.enqueue(event);
+    }
+
+    // Feature Flags
+
+
+
+    private DecideFeatureFlagsResponse getDecide(
+            String distinctId,
+            Map<String, String> groups,
+            Map<String, Object> groupProperties,
+            Map<String, Object> personProperties) {
+        DecideFeatureFlagsRequest decideFeatureFlagsRequest = DecideFeatureFlagsRequest.builder()
+                .v("3")
+                .distinctId(distinctId)
+                .groups(groups)
+                .groupProperties(Optional.ofNullable(groupProperties))
+                .personProperties(Optional.ofNullable(personProperties))
+                .build();
+        return posthogApiClient.featureFlags().decide(decideFeatureFlagsRequest);
     }
 }
